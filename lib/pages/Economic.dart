9866506a34cca7f38ab509f32e9aac07f5739539
Debo15 <40213379+Debo15@users.noreply.github.com>
2022-09-18 23:30:12 +0300

    Implement economic page

--- lib/pages/Economic.dart
+++ lib/pages/Economic.dart
@@ -1,16 +1,85 @@
-// Copyright 2019 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import '../generated/l10n.dart';
+
 
 class EconomicPage extends StatelessWidget {
   const EconomicPage({Key? key}) : super(key: key);
+  final double widthImage = 150;
+  final List<String> IMGs = const <String>[
+    'assets/a man.jfif',
+    'assets/Arrar.jfif',
+    'assets/dehia.jfif',
+    'assets/mansaf.jfif'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("this is the Economic page")),
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Stack(
+            children: [
+              _buildParallaxBackground(context),
+              _buildGradient(),
+              _buildTitleAndSubtitle(context),
+            ],
+          ),
+          const Divider(),
+          Text(
+            S.of(context).irbidEco,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildParallaxBackground(BuildContext context) {
+    return CarouselSlider.builder(
+      itemCount: IMGs.length,
+      itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) =>
+          Container(
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(IMGs[itemIndex], fit: BoxFit.cover)),
+      options: CarouselOptions(
+        autoPlay: true,
+        aspectRatio: 16 / 9,
+        enlargeCenterPage: true,
+        viewportFraction: 1.0,
+      ),
+    );
+  }
+
+
+  Widget _buildGradient() {
+    return Positioned.fill(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.6, 0.95],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitleAndSubtitle(BuildContext context) {
+    return Positioned(
+
+      bottom: 10,
+      child: Text(
+        S.of(context).irbidEco,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
